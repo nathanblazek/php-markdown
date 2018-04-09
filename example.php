27abf0e715a975f4f892644f94fe10d413476512
vagrant <vagrant@scotchbox>
2018-04-09 21:04:19 +0000

    Added basic regex tests, example page for testing functionality and some basic regex based off of my activetype repo. Tests pass and the class is coming along nicely so far.

--- example.php
+++ example.php
@@ -1,5 +1,12 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-$regex = new ncb\Regex('testname','testregex','testreplacement');
-echo $regex->name;
+$textStart = '
+# Test Page
+This is a test to see if the plugin works as intended. 
+-----------------------
+Sure **PHPUnit** can do this, but why not test it, as if I\'m an *end user* as well?!
+';
+$markdown = new ncb\Markdown();
+echo $textStart.'<hr/>';
+echo $markdown->render($textStart);
