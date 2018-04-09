790f777ca82d87af970c92251cd9b74b3326f656
Nathan Blazek <ncblazek@gmail.com>
2018-04-09 21:04:19 +0000

    Added basic regex tests, example page for testing functionality and some basic regex based off of my activetype repo. Tests pass and the class is coming along nicely so far.

--- src/Markdown.php
+++ src/Markdown.php
@@ -1,8 +1,6 @@
 <?php
 namespace ncb;
 
-use Regex;
-
 class Markdown
 {
     private $defaultRegex;
@@ -10,8 +8,33 @@ class Markdown
     public function __construct()
     {
         $this->defaultRegex = array(
-            new Regex('header', '/(#+)(.*)/g)', this::header)
+            new Regex('header', '/(#+)(.*)/',                 'self::header'),
+            new Regex('bold',   '/(\*\*|__)(.*?)\1/',          '<strong>$2</strong>'),
+            new Regex('link',   '/\[([^\[]+)\]\(([^\)]+)\)/',  '<a href="$2">$1</a>'),
+            new Regex('rule',  '/\n-{5,}/',                   '<hr />'),
+            new Regex('code',   '/`(.*?)`/',                   '<code>$1</code>')
         );
+    }
+
+    public function render($markdown){
+        $result = $markdown;
+        if($result){
+            foreach($this->defaultRegex as $regex){
+                if(is_callable($regex->replacement)){
+                    $result = preg_replace_callback($regex->regex,$regex->replacement,$result);
+                } else {
+                    $result = preg_replace($regex->regex,$regex->replacement,$result);
+                }
+            }
+        }
+        return $result;
+    }
+
+    private static function header($matches){
+        if($matches && count($matches)>2) {
+            $size = strlen($matches[1]);
+            return "<h$size>$matches[2]</h$size>";
+        }
     }
 
 }
--- tests/MarkdownTest.php
+++ tests/MarkdownTest.php
@@ -3,8 +3,42 @@ namespace ncb\tests;
 
 use ncb\Markdown;
 class MarkdownTest extends \PHPUnit\Framework\TestCase {
+    private $markdown;
+
+    public function  setUp(){
+        $this->markdown = new Markdown();
+    }
 
     public function testIsClassWorking(){
         $this->assertTrue(true);
+    }
+    public function testHeader(){
+        $headerOne = $this->markdown->render('#Header');
+        $headerTwo = $this->markdown->render('##Header');
+        $headerSix = $this->markdown->render('######Header');
+        $this->assertTrue($headerOne == "<h1>Header</h1>");
+        $this->assertTrue($headerTwo == "<h2>Header</h2>");
+        $this->assertTrue($headerSix == "<h6>Header</h6>");
+
+    }
+
+    public function testBold(){
+        $bold = $this->markdown->render('**Bold Test**');
+        $this->assertTrue($bold == '<strong>Bold Test</strong>');
+    }
+    //link, rule, code
+
+    public function testLink(){
+        $link = $this->markdown->render('[Link](http://www.google.com)');
+        $this->assertTrue($link == '<a href="http://www.google.com">Link</a>');
+    }
+    public function testRule(){
+        $rule = $this->markdown->render("\n--------");
+        $this->assertTrue($rule == '<hr />');
+    }
+
+    public function testCode(){
+        $code = $this->markdown->render("`Test Code`");
+        $this->assertTrue($code == '<code>Test Code</code>');
     }
 }
